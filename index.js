4762b82d6a8515584ff0f2c8d60e9885f6ae4890
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-02 00:35:09 -0800

    commit tree eae59caa369332207d260530e0bc90a885f68aa7

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function createEmployeeRecord(employee) {
+    let record = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return record;
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(createEmployeeRecord)
+}
+
+function createDSObj(getType, dateStamp) {
+    return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+}
+
+function createTimeInEvent(obj, dateStamp){
+    obj.timeInEvents.push(createDSObj("TimeIn", dateStamp))
+    return obj
+}
+
+
+function createTimeOutEvent(obj, dateStamp){
+    obj.timeOutEvents.push(createDSObj("TimeOut", dateStamp))
+    return obj
+}
+
+function hoursWorkedOnDate(obj, dateYMD){
+    const timeIn = obj.timeInEvents.find((e) => e.date === dateYMD).hour
+    const timeOut = obj.timeOutEvents.find((e) => e.date === dateYMD).hour
+    return (timeOut - timeIn)/100
+}
